1c1126dcfa556bbc668e1ac389851e5f6136962f
iExploTech <iexplotech@gmail.com>
2020-05-02 04:51:22 +0800

    Script for check balance of all eth accounts

---
+++ myBalance.js
@@ -0,0 +1,18 @@
+
+function checkAllBalances() {
+    var totalBal = 0;
+    for (var acctNum in eth.accounts) {
+        var acct = eth.accounts[acctNum];
+        var acctBal = web3.fromWei(eth.getBalance(acct), "ether");
+        totalBal += parseFloat(acctBal);
+        console.log("  eth.accounts[" + acctNum + "]: \t" + acct + " \tbalance: " + acctBal + " ether");
+    }
+    console.log("  Total balance: " + totalBal + " ether");
+};
+
+function display() {
+	console.log("IPTM Testnet 2020\nRun this function to check all balances: checkAllBalances()");
+}
+
+display();
+checkAllBalances();
