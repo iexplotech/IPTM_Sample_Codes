0b4f708a3072a0b0ae2b0a3d83e2ace884267d10
iExploTech <iexplotech@gmail.com>
2020-05-11 05:51:45 +0800

    update with IPTM accounts & supply

--- myBalance.js
+++ myBalance.js
@@ -1,25 +1,47 @@
 
 function checkIPTMBalances() {
+	var totalIPTM_Supply = "10000000";
 	var rootAcc = "0x88888bd5756ac3278bc55bb326eb0dc0c96eba60";
 	var defaultAcc = "0x0000706e899d0f46c5efe22c4caaeb885af4dcac";
 	
 	//var sealer_Acc = ["0x1111cad11a99ff45d15be9c1f5abe28758ee681e"];
+	var representative_Acc = [["iExploTech","0x77777aada63106a992ce2a9235bfa1b2f99ebbf4"]];
+	
 	var sealer_Acc = clique.getSigners();
 	
 	console.log("\n  #### IPTM Secretariat Accounts #### ");
 	
-	console.log("  Root    Account, " + rootAcc + ": " + web3.fromWei(eth.getBalance(rootAcc), "ether") + " ether");
-	console.log("  Default Account, " + defaultAcc + ": "+ web3.fromWei(eth.getBalance(defaultAcc), "ether") + " ether");
+	var rootAccBal = web3.fromWei(eth.getBalance(rootAcc), "ether");
+	var defaultAccBal = web3.fromWei(eth.getBalance(defaultAcc), "ether");
 	
-	var totalBal = 0;
+	console.log("  Total IPTM Supply: " + totalIPTM_Supply + " ether");
+	console.log("\n  Root    Account, " + rootAcc + ": " + rootAccBal + " ether");
+	console.log("  Default Account, " + defaultAcc + ": " + defaultAccBal + " ether");
+	
+	var totalRepresentativeBal = 0;
+	for (var i in representative_Acc) {
+		var representative_balance = web3.fromWei(eth.getBalance(representative_Acc[i][1]), "ether");
+		
+		console.log("\n  Representative Account[" + representative_Acc[i][0] + "], " + representative_Acc[i][1] + ": "+ representative_balance + " ether");
+		totalRepresentativeBal+= parseFloat(representative_balance);
+	}
+	
+	console.log("  Total All Representative Accounts Balance: " + totalRepresentativeBal + " ether");
+	
+	var totalSealerBal = 0;
 	for (var i in sealer_Acc) {
 		var sealer_balance = web3.fromWei(eth.getBalance(sealer_Acc[i]), "ether");
 		
 		console.log("\n  Sealer Account[" + i + "], " + sealer_Acc[i] + ": "+ sealer_balance + " ether");
-		totalBal += parseFloat(sealer_balance);
+		totalSealerBal += parseFloat(sealer_balance);
 	}
 	
-	console.log("  Total All Sealer Nodes Balance: " + totalBal + " ether");
+	console.log("  Total All Sealer Nodes Balance: " + totalSealerBal + " ether");
+	
+	var totalAll = parseFloat(rootAccBal) + parseFloat(defaultAccBal) + parseFloat(totalRepresentativeBal) + parseFloat(totalSealerBal);
+	console.log("\n  Total All Above Balance: " + totalAll + " ether");
+	
+	console.log("\n  Circulating IPTM Supply: "  + (parseFloat(totalIPTM_Supply) - parseFloat(totalAll)) + " ether of " + totalIPTM_Supply + " ether");
 };
 
 function checkLocalBalances() {
